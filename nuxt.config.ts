06dc9bb184c4a2eb7984427cffa3e9a133013f6c
Jasouza <jasondsouza2003@gmail.com>
2024-09-14 20:20:07 +0000

    Done init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,33 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: [
+    "~/assets/css/main.css",
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
 })
